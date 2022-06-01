e78d0828055701f4c0950456fd914bf7631d1e37
Anderson Daniel <95385669+RafaBr21@users.noreply.github.com>
2022-06-01 10:57:30 -0300

    card de cada jogo
    (incompleto)

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-//Declarando as variaveis
+//Declarando as variaveis globais
 const global_vars = {
     game_banner: document.querySelector('.game-banner'),
     grid_all_games: document.querySelector("#grid-all-games"),
@@ -32,6 +32,108 @@ const fn_filters = {
     }
 }
 
+const DOM_HTML = { // criar os cards de cada um dos jogos
+    render_div(id, className) {
+        const div = document.createElement("div")
+        if (div) div.setAttribute('id', id)
+        div.classList.add(className)
+        return div;
+    },
+
+    render_link(url) {
+        const a = document.createElement("a")
+        a.href = url
+        a.target = '_blank'
+        return a
+    },
+
+    render_figure(thumbnail, title) {
+        const figure = document.createElement("figure")
+        const game_img = this.render_img(thumbnail, title, 262, 148)
+        const figcaption = document.createElement("figcaption")
+        figcaption.textContent = title
+        figure.appendChild(game_img)
+        figure.appendChild(figcaption)
+        return figure
+    },
+
+    render_img(src, title, width, height) {
+        const img = document.createElement("img")
+        img.src = src
+        img.alt = title
+        if (width && height) {
+            img.width = `${width}`
+            img.height = `${height}`
+        }
+        return img
+    },
+
+    render_input(id) {
+        const input = document.createElement('input')
+        input.id = `${id}`
+        input.type = 'checkbox'
+        return input
+    },
+
+    render_label(className) {
+        const label = document.createElement('label')
+        label.title = "Adicionar/Remover este jogo na sua lista de favoritos"
+        label.classList.add(className)
+        return label
+    },
+
+    render_button(id, className, text) {
+        const button = document.createElement('button')
+        button.setAttribute('id', id)
+        button.classList.add(className)
+        button.textContent = text
+        return button
+    },
+
+    render_banner({ id, thumbnail, title, game_url, genre, platform }) {
+        const game_item = this.render_div(`game${id}`, 'game-item')
+        const game_link = this.render_link(game_url)
+        const game_figure = this.render_figure(thumbnail, title)
+
+        const currentImage = game_figure.firstElementChild //rendereização do primeiro card de jogo
+        currentImage.setAttribute('width', '600')
+        currentImage.setAttribute('height', '250')
+
+        const game_info = this.render_extra_info(id, genre, platform)
+
+        game_item.append(game_link, game_info)
+        game_link.appendChild(game_figure)
+        global_vars.game_banner.appendChild(game_item)
+    },
+
+    render_game_item({ id, thumbnail, title, game_url, genre, platform }) {
+        const game_item = this.render_div(`game-${id}`, 'game-item')
+        const game_link = this.render_link(game_url)
+        const game_figure = this.render_figure(thumbnail, title)
+        const game_info = this.render_extra_info(id, genre, platform)
+
+        game_item.append(game_link, game_info)
+        game_link.appendChild(game_figure)
+
+        global_vars.grid_all_games.appendChild(game_item)
+    },
+
+    render_extra_info(id, genre, platform) {
+        const div = this.render_div(null, 'extra-info')
+        const label = this.render_label('favorite')
+        const checkmark = this.render_div(null, "checkmark_favorite")
+        const input = this.render_input(id)
+        const span = this.rende_span(genre, 'game-genre')
+
+        let existsID = fn_storage.fnExistInStorage(id)
+        if (existsID) input.checked = true;
+
+        div.appendChild(label, span)
+        label.append(input, checkmark)
+        
+        //continuar daqui
+    }
+}
 //Fetch que fetch que faz os jogos aparecerem
 const request_api = {
     options: {
